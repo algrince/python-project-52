84b0b9b831da70f6b38e2c7bda06ea79187eb8be
Ekaterina Mitiashkina <algrince@gmail.com>
2022-12-13 17:38:08 +0100

    add delete

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,5 +4,6 @@ from task_manager.users import views
 urlpatterns = [
     path('', views.UserIndexView.as_view(), name='index'),
     path('create/', views.UserFormCreateView.as_view(), name='user_create'),
-    path('<int:pk>/update/', views.UserFormUpdateView.as_view(), name='user_update')
+    path('<int:pk>/update/', views.UserFormUpdateView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='user_delete'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views import View
 from django.views.generic.base import ContextMixin
 from task_manager.users.models import User
@@ -25,3 +25,9 @@ class UserFormUpdateView(UpdateView):
     model = User
     fields = "__all__"
     template_name = "users/update_user.html"
+
+
+class UserDeleteView(DeleteView):
+
+    model = User
+    template_name = "users/delete_user.html"
