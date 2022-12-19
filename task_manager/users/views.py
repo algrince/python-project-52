d054b43f1ae12130654b20e2e2fb80ccdd707428
Ekaterina Mitiashkina <algrince@gmail.com>
2022-12-19 21:31:30 +0100

    try manage login and permissions

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,48 +1,65 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, AccessMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
 from task_manager.users.models import User
 from task_manager.users.forms import UserForm, ChangeUserForm
 
+
 class UserIndexView(
-    LoginRequiredMixin,
-    ListView
+        ListView
 ):
 
     model = User
-    template_name = "users/index.html"
+    template_name = 'users/index.html'
     context_object_name = "users"
     
 
-class UserFormCreateView(CreateView):
+class UserFormCreateView(
+        SuccessMessageMixin,
+        CreateView
+    ):
 
     model = User
     form_class = UserForm
-    template_name = "users/create_user.html"
+    template_name = 'users/create_user.html'
+
     success_url = reverse_lazy('login')
+    success_message = "%(username)s was created successfully"
 
 
 class UserFormUpdateView(
+        PermissionRequiredMixin,
         LoginRequiredMixin,
-        PermissionRequiredMixin, 
         UpdateView
 ):
 
     model = User
-    permission_required = ('users.change_user')
     form_class = ChangeUserForm
-    template_name = "users/update_user.html"
+    permission_required = 'users.change_user'
+    template_name = ('users/update_user.html')
+
     success_url = reverse_lazy('index')
+    success_message = "%(username)s was updated successfully"
+
+    login_url = 'login'
+    permission_denied_message = _("You can't update other user's account!")
 
 
 class UserDeleteView(
+    PermissionRequiredMixin,
     LoginRequiredMixin,
-    PermissionRequiredMixin, 
     DeleteView,
 ):
 
     model = User
     permission_required = ('users.delete_user')
     template_name = "users/delete_user.html"
+
     success_url = reverse_lazy('index')
+    success_message = "%(username)s was deleted successfully"
+
+    login_url = 'login'
+    permission_denied_message = _("You can't delete other user's account!")
