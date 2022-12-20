6f3d01e844d3347334f6475ad04bee5b2d15217d
Ekaterina Mitiashkina <algrince@gmail.com>
2022-12-20 01:11:54 +0100

    fix update/delete permissions

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, AccessMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
 from task_manager.users.models import User
@@ -31,35 +31,39 @@ class UserFormCreateView(
 
 
 class UserFormUpdateView(
-        PermissionRequiredMixin,
-        LoginRequiredMixin,
-        UpdateView
+        UserPassesTestMixin,
+        SuccessMessageMixin,
+        UpdateView,
 ):
 
     model = User
     form_class = ChangeUserForm
-    permission_required = 'users.change_user'
     template_name = ('users/update_user.html')
 
     success_url = reverse_lazy('index')
     success_message = "%(username)s was updated successfully"
 
     login_url = 'login'
-    permission_denied_message = _("You can't update other user's account!")
+    permission_denied_message = _("You can't change another user!")
+
+    def test_func(self):
+        return self.get_object() == self.request.user
 
 
 class UserDeleteView(
-    PermissionRequiredMixin,
-    LoginRequiredMixin,
-    DeleteView,
+        UserPassesTestMixin,
+        SuccessMessageMixin,
+        DeleteView,
 ):
 
     model = User
-    permission_required = ('users.delete_user')
     template_name = "users/delete_user.html"
 
     success_url = reverse_lazy('index')
     success_message = "%(username)s was deleted successfully"
 
     login_url = 'login'
-    permission_denied_message = _("You can't delete other user's account!")
+    permission_denied_message = _("You can't delete another user!")
+
+    def test_func(self):
+        return self.get_object() == self.request.user
